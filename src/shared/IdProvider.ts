78fcfb0355b95ae167862d037b4708c5368c664c
Rahman <Rahman@seznam.cz>
2015-12-04 15:14:27 +0100

    Added a new file.

---
+++ src/shared/IdProvider.ts
@@ -0,0 +1,18 @@
+﻿/*
+  Part of BrutusNEXT
+
+  Implements generator of unique ids.
+*/
+
+export class IdProvider
+{
+  public generateId(): string
+  {
+    this.myLastIssuedId++;
+
+    // String id is simply a hexadecimal representation of an integer.
+    return this.myLastIssuedId.toString(16);
+  }
+
+  protected myLastIssuedId: number = 0;
+}
