7e882643111a5cde97cd0b59803dae7783ca529d
Rahman <Rahman@seznam.cz>
2015-12-05 22:01:56 +0100

    Another piece of login/auth code.

---
+++ src/server/AuthProcessor.ts
@@ -0,0 +1,158 @@
+﻿/*
+  Part of BrutusNEXT
+
+  Handles user authentication.
+*/
+
+import {ASSERT} from '../shared/ASSERT';
+import {SocketDescriptor} from '../server/SocketDescriptor';
+import {GameServer} from '../server/GameServer';
+import {AccountManager} from '../server/AccountManager';
+
+export class AuthProcessor
+{
+  constructor(protected mySocketDescriptor: SocketDescriptor) {}
+
+  // ---------------- Public methods --------------------
+
+  public processCommand(command: string)
+  {
+    /// TODO
+    switch (this.myStage)
+    {
+      case AuthProcessor.stage.INITIAL:
+        ASSERT(false, "AuthProcessor has not yet been initialized, it is not"
+          + " supposed to process any commands yet");
+        break;
+      case AuthProcessor.stage.LOGIN:
+        this.loginAttempt(command);
+        break;
+      case AuthProcessor.stage.PASSWORD:
+        this.checkPassword(command);
+        break;
+      case AuthProcessor.stage.NEW_PASSWORD:
+        this.getNewPassword(command);
+        break;
+
+      case AuthProcessor.stage.DONE:
+        ASSERT(false, "AuthProcessor has already done it's job, it is not"
+          + " supposed to process any more commands");
+        break;
+      default:
+        ASSERT(false, "Unknown stage");
+        break;
+    }
+  }
+
+  public startLoginProcess()
+  {
+    this.mySocketDescriptor.send("Welcome to the BrutusNext!\r\n"
+      + "By what account name do you want to be recognized? ");
+
+    this.myStage = AuthProcessor.stage.LOGIN;
+  }
+
+  // -------------- Protected class data ----------------
+
+  protected static stage =
+  {
+    INITIAL: 0, // Initial stage.
+    LOGIN: 1,
+    PASSWORD: 2,
+    NEW_PASSWORD: 3,
+    DONE: 4
+  }
+
+  protected myStage = AuthProcessor.stage.INITIAL;
+
+  protected myAccountName = "";
+
+  // --------------- Protected methods ------------------
+
+  protected loginAttempt(accountName: string)
+  {
+    // This regexp will evaluate as true if tested string contains any
+    // non-alphanumeric characters.
+    let regExp = /[^A-Za-z0-9]/;
+
+    if (!accountName)
+    {
+      this.mySocketDescriptor.send(
+        "\r\nYou really need to enter an account name to log in, sorry.\r\n"
+        + "Please enter valid account name: ");
+
+      // We don't advance the stage so the next user input will trigger
+      // a loginAttempt() again.
+      return;
+    }
+
+    if (regExp.test(accountName) === true)
+    {
+      this.mySocketDescriptor.send(
+        "\r\nAccount name can only contain english letters and numbers.\r\n"
+        + "Please enter valid account name: ");
+
+      // We don't advance the stage so the next user input will trigger
+      // a loginAttempt() again.
+      return;
+    }
+
+    if (accountName.length > 20)
+    {
+      this.mySocketDescriptor.send(
+        "\r\nCould you please pick something shorter,"
+        + " like up to 20 characters?.\r\n"
+        + "Please enter valid account name: ");
+
+      // We don't advance the stage so the next user input will trigger
+      // a loginAttempt() again.
+      return;
+    }
+
+    // Make the first lettler uppercase and the rest lowercase.
+    accountName = accountName[0].toUpperCase()
+      + accountName.toLowerCase().substr(1);
+
+    // Remember final account name because nobody else is going to do it.
+    this.myAccountName = accountName;
+
+    let accountManager = GameServer.getInstance().accountManager;
+
+    // We are not going to log in or create a new account just yet,
+    // we will wait for a password.
+    if (accountManager.exists(accountName))
+    {
+      // Existing user. Ask for password.
+      this.mySocketDescriptor.send("Password: ");
+      this.myStage = AuthProcessor.stage.PASSWORD;
+    } else
+    {
+      // New user. Ask for a new password.
+      this.mySocketDescriptor.send("Creating a new user account...\r\n"
+        + "Please enter a password for your account: ");
+      this.myStage = AuthProcessor.stage.NEW_PASSWORD;
+    }
+  }
+
+  protected checkPassword(password: string)
+  {
+    let accountManager = GameServer.getInstance().accountManager;
+
+    if (accountManager.logIn(this.myAccountName, password))
+    {
+      this.myStage = AuthProcessor.stage.DONE;
+    }
+  }
+
+  protected getNewPassword(password: string)
+  {
+    let accountManager = GameServer.getInstance().accountManager;
+
+    /// TODO: Check for password sanity.
+
+    if (accountManager.logIn(this.myAccountName, password))
+    {
+      this.myStage = AuthProcessor.stage.DONE;
+    }
+  }
+}
--- src/server/SocketDescriptor.ts
+++ src/server/SocketDescriptor.ts
@@ -4,16 +4,294 @@
   Implements container for information associated to a socket.
 */
 
+import {ASSERT} from '../shared/ASSERT';
+import {Mudlog} from '../server/Mudlog';
+import {GameServer} from '../server/GameServer';
+import {AuthProcessor} from '../server/AuthProcessor';
+
+// Built-in node.js modules.
 import * as net from 'net';  // Import namespace 'net' from node.js
+
+/*
+enum UserState { AUTHENTICATION, LOBBY, ONLINE };
+*/
 
 export class SocketDescriptor
 {
-  public get socket() { return this.mySocket; }
-
   constructor
-  (
+    (
     protected mySocket: net.Socket,
     protected myId
-  )
+    )
   { }
+
+  // ----------------- Public data ----------------------
+
+  public get socket() { return this.mySocket; }
+
+  // ---------------- Public methods --------------------
+
+  public startLoginProcess()
+  {
+    this.myAuthProcessor.startLoginProcess();
+  }
+
+  public socketReceivedData(data: string)
+  {
+    console.log("SocketDescriptor.socketReceivedData()");
+
+    data = this.normalizeCRLF(data);
+
+    // Do not parse protocol data if user sent just an empty newline.
+    if (data !== "" && data !== '\r\n')
+    {
+      if ((data = this.parseProtocolData(data)) == "")
+        return;
+    }
+
+    console.log("Data after protocol parse: " + data);
+
+    // Discard data that doesn't contain a newline. That's probably
+    // some protocol negotiation stuff we haven't been able to catch.
+    // (slice(-2) creates a substring starting at two characters from the end)
+    if (data.slice(-2) !== '\r\n')
+    {
+      Mudlog.log(
+        "Received (and discarded) data of unknown format not ending with"
+        + " newline (comming from socket descriptor '"
+        + this.myId + "'): " + data,
+        Mudlog.msgType.SYSTEM_INFO,
+        Mudlog.levels.IMMORTAL);
+      return;
+    }
+
+    // Array to contain each line of input as a separate string
+    let lines: Array<string> = [];
+
+    // If input contains multiple lines, split them.
+    // (data.slice(0, -2) will create a sumbstring not containing last two
+    // characters, which are '\r\n' because that's what we checked for right
+    // before)
+    if (data.slice(0, -2).indexOf('\r\n') !== -1)
+    {
+      lines = data.split('\r\n');
+    } else
+    {
+      lines.push(data);
+    }
+
+    // Handle each line as a separate command. Also trim it (remove leading
+    // and trailing white spaces) before processing.
+    for (let i = 0; i < lines.length; i++)
+      this.processCommand(lines[i].trim());
+  }
+
+  public socketClose()
+  {
+    /// TODO
+    console.log("SocketDescriptor.socketClose()");
+  }
+
+  public socketError()
+  {
+    /// TODO: Poslat nejake info hraci, ze se stalo neco zleho.
+    /// (zatim naprosto netusim, pri jake prilezitosti muze socket error
+    /// nastat)
+  }
+
+  // Sends a string to the user.
+  public send(data: string)
+  {
+    this.mySocket.write(data);
+  }
+
+  // -------------- Protected class data ----------------
+
+  // Empty string means that we do not yet know what account does this
+  // descriptor match to.
+  protected myAccountId = "";
+
+  // Buffer to accumulate incomplete parts of data stream.
+  protected myBuffer = "";
+
+  protected myAuthProcessor = new AuthProcessor(this);
+
+  /*
+  protected myUserState: UserState = UserState.AUTHENTICATION;
+  */
+
+  // -------------- Protected methods -------------------
+
+  // Parses and executes a single-line command.
+  protected processCommand(command: string)
+  {
+    if (this.myAccountId === "")
+    {
+      this.myAuthProcessor.processCommand(command);
+    } else
+    {
+      GameServer.getInstance().accountManager.
+        getAccount(this.myAccountId).processCommand(command);
+    }
+
+    /*
+    switch (this.myUserState)
+    {
+      case UserState.AUTHENTICATION:
+        ///this.myAuthenticationProcessor.processCommand();
+        break;
+
+      case UserState.LOBBY:
+        ///this.myLobbyProcessor.processCommand();
+        break;
+
+      case UserState.ONLINE:
+        ///this.myCharacter.processCommand();
+        break;
+
+      default:
+        ASSERT(false, "Unknown user state");
+        break;
+    }
+    */
+  }
+
+  // Ensures that all newlines are in format CR;LF ('\r\n');
+  protected normalizeCRLF(data: string)
+  {
+    if (data && data.length)
+      data = data.replace(/(\r\n|\n\r)/gi, '\n').replace(/\n/gi, '\r\n');
+
+    return data;
+  }
+
+  // Processes any protocol-specific data and removes it from the data stream.
+  protected parseProtocolData(data: string)
+  {
+    data = this.myBuffer + data;
+
+    if (data.indexOf('\xff\xfb\xc9') !== -1)
+    {
+      // IAC WILL GMCP
+      data = data.replace('\xff\xfb\xc9', '');
+
+      /// GMCP asi chci, ale ten event se nikde nezpracovava :\
+      ///user.emit('will.gmcp', this);
+    }
+ 
+    if (data.indexOf('\xff\xfd\xc9') !== -1)
+    {
+      // IAC DO GMCP
+      data = data.replace('\xff\xfd\xc9', '');
+
+      /// GMCP asi chci, ale ten event se nikde nezpracovava :\
+      ///user.emit('do.gmcp', this);
+    }
+
+    if (data.indexOf('\xff\xfd\x5b') !== -1)
+    { // IAC DO MXP
+      ///log('got IAC DO MXP');
+      data = data.replace('\xff\xfd\x5b', '');
+      /// MXP ani nevim, co je...
+      ///user.emit('do.mxp', this);
+    }
+
+    if (data.indexOf('\xff\xfc') !== -1)
+    {
+      // IAC WON'T *
+      data = data.replace(/\xff\xfc./g, '');
+    }
+
+    if (data.indexOf('\xff\xfa\xc9') !== -1)
+    {
+      if (data.indexOf('\xff\xf0') === -1)
+      {
+        /// Ok, tohle snad jakz takz chapu: Nedosel cely GMCP packet, takze si
+        /// data odlozime do bufferu, dokud nedojde zbytek.
+        ///log('incomplete GMCP package', this);
+        this.myBuffer = data;
+        return "";
+      }
+
+      let j = data.match(/\xff\xfa\xc9([^]+?)\xff\xf0/gm);
+
+      if (j && j.length)
+      {
+        for (let i = 0; i < j.length; i++)
+        {
+          /// Zas naprosto netusim, co tohle znamena a jak se ma zpracovat
+          //// event 'gmcp'
+          ///log('GMCP: ' + j[i].slice(3, -2), this);
+          ///user.emit('gmcp', this, j[i].slice(3, -2));
+          data = data.replace(j[i], '');
+        }
+      }
+
+      if (data.indexOf('\xff\xfa\xc9') !== -1)
+      {
+        this.myBuffer = data;
+        return "";
+      }
+      else
+        this.myBuffer = '';
+    }
+
+ /// JSON zatim nepotrebuju
+ /*
+    if ((data.indexOf('{') !== -1 || data.indexOf('}') !== -1))
+    {
+
+      let json;
+
+      if ((json = data.match(/\{[\s\S]+\}/g)))
+      {
+				
+        //log('possible json in user input');
+        for (let i in json)
+        {
+          if (this.receiveJSON(json[i]));
+          data = data.replace(json[i], '');
+        }
+      }
+
+      if (data && data.length)
+      { 
+        /// to do: put a limit on how long we wait for a complete json object,
+        /// and process pending input another way
+        if ((data.indexOf('{') !== -1 || data.indexOf('}') !== -1))
+        {
+          this.myBuffer = data;
+          return "";
+        }
+      }
+    }
+  */
+
+    // Nechapu, proc to nedela nic jineho, nez ze to ze streamu odmaze
+    // prislusny kod...
+    if (data.indexOf('��*') !== -1)
+    {
+      // IAC WILL CHARSET
+      ///log('IAC WILL CHARSET - negotiating UTF8');
+      data = data.replace('��*', '');
+      //this.send(my().PROTOCOL.WILL_UTF8);
+    }
+
+    if (data.search(/[^\x00-\x7f]/) !== -1)
+    {
+      /*
+      log('unsupported char range: ' + data.split('').map(function(i)
+      {
+        let n = i.charCodeAt(0);
+        return my().PROTOCOL[n] || i;
+      }).join(', '), this);
+      */
+
+      /// Like wtf?
+      data = data.replace(/\xff\xfa[^]+?\xff\xf0/gm, '');
+      data = data.replace(/\xff../g, '');
+    }
+
+    return data.trim().length ? data : "";
+  }
 }
