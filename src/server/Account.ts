43be56c8411a28a47c9379fffa6509455cd929fa
Rahman <Rahman@seznam.cz>
2015-12-05 22:01:56 +0100

    Another piece of login/auth code.

---
+++ src/server/Account.ts
@@ -0,0 +1,20 @@
+﻿/*
+  Part of BrutusNEXT
+
+  Implements player account.
+*/
+
+export class Account
+{
+  constructor(protected myAccountName) { }
+  // ---------------- Public methods --------------------
+
+  public processCommand(command: string)
+  {
+    /// TODO
+  }
+
+  // -------------- Protected class data ----------------
+
+  // --------------- Protected methods ------------------
+}
