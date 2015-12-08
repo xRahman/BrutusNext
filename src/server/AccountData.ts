16304e74e021cd1fa8e7fb356743caa61d5c0dca
Rahman <Rahman@seznam.cz>
2015-12-08 22:25:11 +0100

    More saving/loading functionality (contains some test code atm.)

---
+++ src/server/AccountData.ts
@@ -0,0 +1,15 @@
+﻿/*
+  Part of BrutusNEXT
+
+  Holds saveable data members for Account class.
+*/
+
+import {SaveableObject} from '../shared/SaveableObject';
+
+export class AccountData extends SaveableObject
+{
+  public blah = "BLAH";
+  public x = 0;
+  public struct = { kafe: "Kafe", teplota: 13 };
+  public pole = [1, 2, 3, 4];
+}
