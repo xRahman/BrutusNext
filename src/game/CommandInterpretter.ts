4d619e70d25e5d35c88177006a8918e64ccd3762
Rahman <Rahman@seznam.cz>
2015-12-13 21:47:16 +0100

    Added files ommited by microsoft git

---
+++ src/game/CommandInterpretter.ts
@@ -0,0 +1,118 @@
+﻿/*
+  Part of BrutusNEXT
+
+  TODO.
+*/
+
+
+/*
+import {ASSERT_FATAL} from '../shared/ASSERT';
+import {Server} from '../server/Server';
+import {AccountManager} from '../server/AccountManager';
+*/
+
+import {ASSERT} from '../shared/ASSERT';
+
+// This is a declaration of type that is a function that gets string as
+// parameter (the rest of command string without actual command) and doesn't
+// return anything.
+export interface CommandHandler
+{
+  (argument: string): void;
+}
+
+export class CommandInterpretter
+{
+
+  // ---------------- Public methods --------------------
+
+  // Returns true if command is known and handled.
+  public processCommand(commandString: string): boolean
+  {
+    if (!ASSERT(commandString !== "", "Attempt to process empty command"))
+      return false;
+
+    let argument = "";
+    // Parse first word (the actual command) from command string
+    let command = commandString;
+
+    // If there is a space in commandString, command will be it's substring
+    // from the begenning to the position of first space.
+    if (commandString.indexOf(' ') !== -1)
+      command = commandString.substring(0, commandString.indexOf(' '));
+
+    // Now cycle through all commands known to this command interpretter.
+    for (let i = 0; i < this.myCommands.length; i++)
+    {
+      // Here we handle abbreviations by only comparing as many characters
+      // of know command as is the length of received command abbreviation.
+      if (this.myCommands[i].substring(0, command.length) === command)
+      {
+        // Here we parse command argument(s), which is the rest of the
+        // command string when command is cut off of it.
+        if (commandString.indexOf(' ') === -1)
+        {
+          // If there is no space in commandString, argument will be empty
+          // string.
+          argument = "";
+        }
+        else
+        {
+          // Otherwise it's going to be substring beginnig one character
+          // after the position of first space and ending at the end of
+          // commandString.
+          argument = commandString
+            .substring(commandString.indexOf(' ') + 1, commandString.length);
+        }
+
+        // We have matched the command, so it's time to call respective
+        // command handler.
+        this.myCommandHandlers[i](argument);
+
+        return true;
+      }
+    }
+
+    // If we ended up here, it means that commandString matched none of our
+    // know commands.
+    return false;
+  }
+
+  // This method allows you to add a new command to your game entity.
+  // IMPORTANT: You need to pass handler using lambda function or it
+  // won't have correct 'this'.
+  // Example:
+  //   this.myCommandInterpretter.registerCommand
+  //   (
+  //     "sit",
+  //     (argument) => { this.onSit(argument); }
+  //   );
+  public registerCommand(command: string, handler: CommandHandler)
+  {
+    if (!ASSERT(command !== "", "Attempt to register empty command"))
+      return;
+
+    if (!ASSERT(!(command in this.myCommands),
+          "Attempt to register a command that is already registered"))
+      return;
+
+    this.myCommands.push(command);
+    this.myCommandHandlers.push(handler);
+  }
+
+  // -------------- Protected class data ----------------
+
+  // Array containing commands this entity knows. It needs to be array
+  // in order for order to matter (which is necessary to correctly match
+  // abbreviations)
+  protected myCommands: Array<string> = [];
+  // Array containing command handlers at the same indexes as corresponding
+  // commands have in myCommands array.
+  protected myCommandHandlers: Array<CommandHandler> = [];
+
+
+  // --------------- Protected methods ------------------
+
+  // ---------- Auxiliary protected methods ------------- 
+
+}
---
+++ src/server/PlayerConnection.ts
@@ -0,0 +1,227 @@
+﻿/*
+  Part of BrutusNEXT
+
+  A single connection to the game.
+*/
+
+import {ASSERT} from '../shared/ASSERT';
+import {Id} from '../shared/Id';
+import {Mudlog} from '../server/Mudlog';
+import {Server} from '../server/Server';
+import {SocketDescriptor} from '../server/SocketDescriptor';
+import {AuthProcessor} from '../server/AuthProcessor';
+import {LobbyProcessor} from '../server/LobbyProcessor';
+import {GameEntity} from '../game/GameEntity';
+
+export class PlayerConnection
+{
+  constructor(protected mySocketDescriptor: SocketDescriptor) { }
+
+  // ----------------- Public data ----------------------
+
+  ///public get socket() { return this.mySocket; }
+
+  public set id(value: Id) { this.myId = value; }
+  public get id() { return this.myId; }
+
+  public set accountId(value: Id) { this.myAccountId = value; }
+  public get accountId() { return this.myAccountId; }
+
+  // ------- Internal stage transition methods ----------
+
+  public startLoginProcess()
+  {
+    ASSERT(this.myStage === PlayerConnection.stage.INITIAL,
+      "Starting login process from wrong stage");
+
+    this.myStage = PlayerConnection.stage.AUTHENTICATION;
+    this.myAuthProcessor.startLoginProcess();
+  }
+
+  public enterLobby()
+  {
+    ASSERT(this.myStage === PlayerConnection.stage.AUTHENTICATION
+      || this.myStage === PlayerConnection.stage.IN_GAME,
+      "Entering lobby from wrong stage");
+
+    this.myStage = PlayerConnection.stage.LOBBY;
+    this.myLobbyProcessor.enterMenu();
+  }
+
+  public enterGame()
+  {
+    ASSERT(this.myAccountId !== null, "Invalid account id");
+
+    let accountManager = Server.accountManager;
+    let accountName = accountManager.getAccount(this.myAccountId).accountName;
+
+    this.myStage = PlayerConnection.stage.IN_GAME;
+
+    // For now there can only be one character per account
+    // so we don't need to present choice which one to log in with.
+    //   Entering game currently means logging in with the character
+    // associated to the account.
+    if (Server.game.characterManager.exists(accountName))
+    {
+      /// TODO
+      /// Load existing character and enter game.
+
+      /*
+      if (loadCharacterFailed())
+      {
+        // TODO: send explaining message to the player.
+        this.enterLobby();
+      }
+      */
+    }
+    else
+    {
+      /// TODO
+      /// Create a new character and enter game.
+    }
+  }
+
+  public quitGame()
+  {
+    this.myStage = PlayerConnection.stage.QUITTED_GAME;
+    // Disconnect the player. Event 'close' will be generated on the socket,
+    // which will lead to the player being logged out.
+    this.mySocketDescriptor.disconnect();
+  }
+
+  // ---------------- Public methods --------------------
+
+  // Parses and executes a single-line command.
+  public processCommand(command: string)
+  {
+    switch (this.myStage)
+    {
+      case PlayerConnection.stage.INITIAL:
+        ASSERT(false, "PlayerConnection has not yet been initialized by"
+          + "startLoginProcess(), it is not supposed to process any"
+          + " commands yet");
+        break;
+      case PlayerConnection.stage.AUTHENTICATION:
+        ASSERT(this.myAccountId === null,
+          "Attempt to start authentication on player connection that already"
+          + " has an online account");
+
+        this.myAuthProcessor.processCommand(command);
+        break;
+      case PlayerConnection.stage.LOBBY:
+        ASSERT(this.myAccountId !== null,
+          "Attempt to process lobby command on player connection that doesn't"
+          + " have an online account");
+
+        this.myLobbyProcessor.processCommand(command);
+        break;
+      case PlayerConnection.stage.IN_GAME:
+        ASSERT(this.myAccountId !== null,
+          "Attempt to process ingame command on player connection that doesn't"
+          + " have an online account");
+
+        /// TODO
+        /// Tohle je asi taky blbost. Commandy musej umet zpracovatvat primo
+        /// mud entity (charactery a tak), aby se pres ne daly ovladat
+        /// ze scriptu a tak. Mit naveseny command processor na player
+        /// connection je asi zbytecny.
+        ///  Tzn. tady to rovnou poslat na online character (respektive
+        /// online entitu, protoze to muze byt cokoliv, ne nutne jen character)
+        /// (je v principu mozne se switchnout do roomy, objectu, atd.
+        /// a davat tomu herni prikazy).
+///        this.myGameEntity.processCommand(command);
+        /// TESTING:
+        this.tmpCharacter.processCommand(command);
+        break;
+      case PlayerConnection.stage.QUITTED_GAME:
+        ASSERT(false, "Player has quitted the game, PlayerConnection"
+          + " is not supposed to process any more commands");
+        break;
+      default:
+        ASSERT(false, "Unknown stage");
+        break;
+    }
+  }
+
+  // Close the connection
+  public close()
+  {
+    if (this.myStage === PlayerConnection.stage.IN_GAME)
+    {
+      /// TODO
+      // Player was in game and his link has just died.
+    }
+    else
+    {
+      // Player was not in game, log him off.
+      // (but only if an account is already associated to this player
+      // connection. It might not be, for example if player closed his
+      // connection before entering password.)
+      if (this.myAccountId)
+      {
+        Server.accountManager.logOut(this.myAccountId);
+        this.myAccountId = null;
+      }
+      else
+      {
+        let address = this.mySocketDescriptor.remoteAddress;
+        let player = "";
+
+        if (this.myAuthProcessor.accountName)
+        {
+          player = "Player " + this.myAuthProcessor.accountName;
+        }
+        else
+        {
+          player = "Unknown player";
+        }
+
+        Mudlog.log(
+          player + " closed connection before logging in",
+          Mudlog.msgType.SYSTEM_INFO,
+          Mudlog.levels.IMMORTAL);
+      }
+    }
+
+    Server.playerConnectionManager.removePlayerConnection(this.myId);
+  }
+
+  // Sends a string to the user.
+  public send(data: string)
+  {
+    this.mySocketDescriptor.send(data);
+  }
+
+  public isInGame()
+  {
+    return this.myStage === PlayerConnection.stage.IN_GAME;
+  }
+
+  // -------------- Protected class data ----------------
+
+  // Empty string means that we do not yet know what account does this
+  // descriptor match to.
+  protected myAccountId: Id = null;
+
+  // Unique stringId of this descriptor.
+  protected myId: Id = null;
+
+  protected myAuthProcessor = new AuthProcessor(this);
+  protected myLobbyProcessor = new LobbyProcessor(this);
+
+  protected static stage =
+  {
+    INITIAL: 0, // Initial stage.
+    AUTHENTICATION: 1,
+    LOBBY: 2,
+    IN_GAME: 3,
+    QUITTED_GAME: 4
+  }
+
+  protected myStage = PlayerConnection.stage.INITIAL;
+
+  // -------------- Protected methods -------------------
+
+  /// TESTING
+  tmpCharacter = new GameEntity({ version: 0 });
+}
