5e5b37b0b3a029ee361cab0fc8c796906a62c871
Rahman <Rahman@seznam.cz>
2016-03-19 11:45:38 +0100

    Removed SaveableContainer, SaveableObject is used instead (data that should not be saved should be placed to non-saveable sub-conainer, instead of placing data that should be saved into saveable sub-container. This removes the need to have XXXData class for every XXX class.) Files are not removet yet in this commit.
    Added test generation of world, realm zone and room. It's not working as intended yet.
    
    Added base of support for GameEntities to be able to contain other game entities. It's not working yet, thanks to bug in V8 engine disallowing using super. in async functions. Some workaround will be necessary.

--- src/shared/Id.ts
+++ src/shared/Id.ts
@@ -12,7 +12,7 @@ import {SaveableObject} from '../shared/SaveableObject';
 
 export class Id extends SaveableObject
 {
-  // myStringID should be unique per boot.
+  // myStringID is a unique string.
   // myType is the name of the class this id points to.
   constructor(protected myStringId: string, protected myType: string)
   {
@@ -22,27 +22,12 @@ export class Id extends SaveableObject
     // properties. You will also need to convert data in respective
     // .json files to conform to the new version.
     this.version = 0;
-
-    // Ids are saved to json by default.
-    this.isSaved = true;
   }
 
   // New object is returned so you can use this to inicialize an id that will
   // be loaded from file later (if it was static value, loading multiple
   // 'uninitialized' ids would overwrite each other).
   public static get NULL() { return new Id("", ""); }
-
-  /*
-  // Use SAVED_NULL if you want your null id saved to json.
-  public static get SAVED_NULL()
-  {
-    let nullId = new Id("", "");
-
-    nullId.isSaved = true;
-
-    return nullId;
-  }
-  */
 
   public get stringId() { return this.myStringId; }
   public get type() { return this.myType; }
