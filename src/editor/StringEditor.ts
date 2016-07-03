62b99e54d027a51a684134ef5ab2ea4805c196d7
Rahman <Rahman@seznam.cz>
2016-07-03 11:38:37 +0200

    Added handling of multiple alias keyword to AbbrevSearchList (this commit contains commented unfinished torso of more complicated alorith)

---
+++ src/editor/StringEditor.ts
@@ -0,0 +1,64 @@
+/*
+  Part of BrutusNEXT
+
+  Allows to edit text in terminal mode.
+*/
+
+'use strict';
+
+import {ASSERT} from '../shared/ASSERT';
+import {ASSERT_FATAL} from '../shared/ASSERT';
+import {PlayerConnection} from '../server/PlayerConnection';
+
+export class StringEditor
+{
+  constructor
+  (
+    private playerConnection: PlayerConnection,
+    initialText: string
+  )
+  {
+    this.text = initialText;
+    this.origText = initialText;
+  }
+
+  // -------------- Protected class data ----------------
+
+  // -------------- Private class data ----------------
+
+  // Text that is being edited.
+  private text = "";
+  private origText = "";
+
+  // -------------- Public methods -------------------
+
+  public processCommand(command: string)
+  {
+    switch (command)
+    {
+      case '/s':  // Save the result and exit the editor.
+        this.playerConnection.returnFromStringEditor();
+      break;
+
+      case '/a':  // Discard all changes and exit the editor.
+        this.text = this.origText;
+        this.playerConnection.abortStringEditor();
+      break;
+
+      case '/c':  // Clear the contents.
+        this.text = "";
+      break;
+
+      default:    // Append the text.
+        this.text += command;
+      break;
+    }
+  }
+
+  // -------------- Protected methods -------------------
+
+
+  // --------------- Private methods --------------------
+
+
+}
---
+++ src/shared/EditableObject.ts
@@ -0,0 +1,38 @@
+/*
+  Part of BrutusNEXT
+
+  Implements functionality that enables inherited classes to have their
+  properties edited.
+*/
+
+'use strict';
+
+import {ASSERT} from '../shared/ASSERT';
+import {ASSERT_FATAL} from '../shared/ASSERT';
+import {AttributableClass} from '../shared/AttributableClass';
+/*
+import {NamedClass} from '../shared/NamedClass';
+import {Mudlog} from '../server/Mudlog';
+import {Server} from '../server/Server';
+
+// Built-in node.js modules.
+import * as fs from 'fs';  // Import namespace 'fs' from node.js
+
+// 3rd party modules.
+let promisifiedFS = require('fs-promise');
+let beautify = require('js-beautify').js_beautify;
+*/
+
+export class EditableObject extends AttributableClass
+{
+
+  // -------------- Protected class data ----------------
+
+
+  // -------------- Protected methods -------------------
+
+
+  // --------------- Private methods --------------------
+
+
+}
