8436c2b3223bb41da610e7992587c5ab098106d7
Rahman <Rahman@seznam.cz>
2017-01-17 14:47:13 +0100

    Bugfixing

---
+++ src/client/components/ScrollViewOutput.ts
@@ -0,0 +1,95 @@
+/*
+  Part of BrutusNEXT
+
+  Implements output element of scrollable text window.
+*/
+
+'use strict';
+
+import MudColorComponent = require('../components/MudColorComponent');
+
+import $ = require('jquery');
+
+class ScrollViewOutput extends MudColorComponent
+{
+  public static get CSS_CLASS() { return 'ScrollViewOutput'; }
+
+  // -------------- Static class data -------------------
+
+  //----------------- Protected data --------------------
+
+  protected $output = null;
+
+  //------------------ Private data ---------------------
+
+  // --------------- Static accessors -------------------
+
+  // ---------------- Static methods --------------------
+
+  // --------------- Public accessors -------------------
+
+  // ---------------- Public methods --------------------
+
+  // -> Returns created jquery element.
+  public create()
+  {
+    // Create a DOM element.
+    let output = this.createDivElement
+    (
+      this.id,
+      ScrollViewOutput.CSS_CLASS
+    );
+
+    // Create jquery element from the DOM element.
+    this.$output = $(output);
+
+    return this.$output;
+  }
+
+  // Converts message from mud colors (like '&Rhealth&g') to html
+  // reprezentation (using <span> elements) and adds the result to
+  // the end of output component.
+  public appendMessage(message: string)
+  {
+    let html = this.htmlizeMudColors(message);
+
+    this.appendHtml(html, { forceScroll: false });
+  }
+
+  // If 'param.forceScroll' is 'true', output always scrolls to the bottom.
+  // Otherwise it only scrolls if user hasn't scrolled up manually.
+  public appendHtml(html: string, param: { forceScroll: boolean })
+  {
+    // Size of the scrollable range (in pixels).
+    let range =
+      this.$output.prop('scrollHeight') - this.$output.prop('clientHeight');
+
+    // 'true' if user has scrolled up manually
+    // (-1 to account for rounding errors. If use scolled up by
+    //  less than one pixel, we can safely scoll back down anyways).
+    let userScrolled = (this.$output.scrollTop()) < (range - 1);
+
+    this.$output.append(html);
+
+    // If user scolls up manually, we don't want to scroll
+    // the output down to the bottom on each output, that
+    // would be very inconvinient.
+    if (param.forceScroll || !userScrolled)
+      this.scrollToBottom();
+  }
+
+  // --------------- Protected methods ------------------
+
+  // ---------------- Private methods -------------------
+
+  public scrollToBottom()
+  {
+    // Scroll to bottom.
+    this.$output.scrollTop(this.$output.prop('scrollHeight'));
+  }
+
+  // ---------------- Event handlers --------------------
+
+}
+
+export = ScrollViewOutput;
