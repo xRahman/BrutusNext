c742a75be93d2c4d973fbc9372f8f6abebbbb35d
Rahman <Rahman@seznam.cz>
2017-02-01 00:58:29 +0100

    Starting to play with mapper

---
+++ src/client/mapper/Room.ts
@@ -0,0 +1,37 @@
+/*
+  Part of BrutusNEXT
+
+  Holds map data for a single room.
+*/
+
+'use strict';
+
+///import Connection = require('../connection/Connection');
+
+class Room
+{
+  // -------------- Static class data -------------------
+
+  //----------------- Protected data --------------------
+
+  //------------------ Private data ---------------------
+
+  name: string;
+
+  // --------------- Static accessors -------------------
+
+  // ---------------- Static methods --------------------
+
+  // --------------- Public accessors -------------------
+
+  // ---------------- Public methods --------------------
+
+  // --------------- Protected methods ------------------
+
+  // ---------------- Private methods -------------------
+
+  // ---------------- Event handlers --------------------
+
+}
+
+export = Room;
