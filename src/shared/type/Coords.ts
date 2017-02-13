144d42b8122f435a47f0e4a0407cc3ef0bc901f9
Rahman <Rahman@seznam.cz>
2017-02-13 14:54:02 +0100

    Workin on new client data structure

--- src/shared/type/Coords.ts
+++ src/shared/type/Coords.ts
@@ -8,6 +8,16 @@
 
 export class Coords
 {
+  // Public overloads.
+  constructor()
+  constructor(x: number, y: number, z: number)
+  // Overloads implementation (not public).
+  constructor(x?: number, y?: number, z?: number)
+  {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+  }
 
   // -------------- Static class data -------------------
 
