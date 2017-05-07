2ca1f0c4d545f52b0f40ae5a5b3bf21b855630af
Rahman <Rahman@seznam.cz>
2017-05-07 18:27:24 +0200

    Recursive loading of prototypes

--- src/shared/lib/entity/PrototypeManager.ts
+++ src/shared/lib/entity/PrototypeManager.ts
@@ -10,6 +10,7 @@ import {Entity} from '../../../shared/lib/entity/Entity';
 
 export class PrototypeManager
 {
+  /// TODO: Asi by spíš celý PrototypeManager měl být name search list.
   // Key:   prototype name (which is the entity name)
   // Value: prototype entity
   protected prototypes = new Map<string, Entity>();
@@ -17,7 +18,8 @@ export class PrototypeManager
   // Initializes root prototype entities.
   public async initPrototypes(entityClasses: Array<string>)
   {
-    // There is nothing to be done on the client, all prototype
-    // entities are sent along with other entities.
+    // Nothing by default.
+    // (There is nothing to be done on the client, all prototype
+    //  entities are sent along with other entities.)
   }
 }
