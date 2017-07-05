6335d2b7e5e435338c2a2880c6b5c36acc3d5a65
Rahman <Rahman@seznam.cz>
2017-07-05 23:22:22 +0200

    Messing with login.

---
+++ src/shared/lib/protocol/LoginResponse.ts
@@ -0,0 +1,45 @@
+/*
+  Part of BrutusNEXT
+
+  Part of client-server communication protocol.
+
+  Server response to login request.
+*/
+
+import {ERROR} from '../../../shared/lib/error/ERROR';
+import {Packet} from '../../../shared/lib/protocol/Packet';
+import {EntityData} from '../../../shared/lib/protocol/EntityData';
+import {Classes} from '../../../shared/lib/class/Classes';
+
+export class LoginResponse extends Packet
+{
+  constructor()
+  {
+    super();
+
+    this.version = 0;
+  }
+
+  // Is the request accepted?
+  public result: LoginResponse.Result = LoginResponse.Result.OK;
+
+  // Description of problem if the request is denied.
+  public problem: string = null;
+
+  // Serialized account data.
+  public account = new EntityData();
+}
+
+// ------------------ Type declarations ----------------------
+
+export module LoginResponse
+{
+  export enum Result
+  {
+    OK,
+    AUTHENTICATION_FAILED,
+    FAILED_TO_LOAD_ACCOUNT
+  }
+}
+
+Classes.registerSerializableClass(LoginResponse);
