ee5782e6a65948790fe29b220370eaa37cd4fa3e
Rahman <Rahman@seznam.cz>
2017-07-11 15:05:29 +0200

    Some fixes.

--- src/client/gui/StandaloneWindow.ts
+++ src/client/gui/StandaloneWindow.ts
@@ -22,15 +22,8 @@ export class StandaloneWindow extends Window
     { return 'S_StandaloneWindow_Title'; }
   protected static get CONTENT_S_CSS_CLASS()
     { return 'S_StandaloneWindow_Content'; }
-  // protected static get TEXT_S_CSS_CLASS()
-  //   { return 'S_StandaloneWindow_Text'; }
-  // protected static get LINK_TEXT_S_CSS_CLASS()
-  //   { return 'S_StandaloneWindow_LinkText'; }
   protected static get LINK_CONTAINER_S_CSS_CLASS()
     { return 'S_StandaloneWindow_LinkContainer'; }
-  /// To be deleted.
-  // protected static get EMPTY_LINE_S_CSS_CLASS()
-  //   { return 'S_StandaloneWindow_EmptyLine'; }
 
   // -------------- Static class data -------------------
 
@@ -117,54 +110,6 @@ export class StandaloneWindow extends Window
       }
     );
   }
-
-  // protected createText
-  // (
-  //   {
-  //     $container,
-  //     text,
-  //     sCssClass = StandaloneWindow.TEXT_S_CSS_CLASS
-  //   }:
-  //   {
-  //     $container: JQuery;
-  //     text: string;
-  //     sCssClass?: string;
-  //   }
-  // )
-  // {
-  //   return Component.createText
-  //   (
-  //     {
-  //       $container: $container,
-  //       sCssClass: sCssClass,
-  //       text: text
-  //     }
-  //   );
-  // }
-
-  // protected createLinkText
-  // (
-  //   {
-  //     $container,
-  //     text,
-  //     sCssClass = StandaloneWindow.LINK_TEXT_S_CSS_CLASS
-  //   }:
-  //   {
-  //     $container: JQuery;
-  //     text: string;
-  //     sCssClass?: string;
-  //   }
-  // )
-  // {
-  //   return Component.createTextLink
-  //   (
-  //     {
-  //       $container: $container,
-  //       sCssClass: sCssClass,
-  //       text: text
-  //     }
-  //   );
-  // }
 
   // ---------------- Private methods -------------------
 
