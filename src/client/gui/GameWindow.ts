1115b08eb9245dea45ab8aa5cf5889698dd69c61
Rahman <Rahman@seznam.cz>
2017-07-12 16:54:02 +0200

    Messing with GUI.

--- src/client/gui/GameWindow.ts
+++ src/client/gui/GameWindow.ts
@@ -37,21 +37,16 @@ export class GameWindow extends Window
   // ~ Overrides Window.create().
   public create
   (
-    window_sCssClass = GameWindow.S_CSS_CLASS,
-    content_sCssClass = Window.CONTENT_S_CSS_CLASS,
-    titleBar_sCssClass = GameWindow.TITLE_BAR_S_CSS_CLASS,
-    title_sCssClass = GameWindow.TITLE_S_CSS_CLASS,
+    param: Window.CreateParam =
+    {
+      window_sCssClass: GameWindow.S_CSS_CLASS,
+      ///contentCss: { sClass: Window.CONTENT_S_CSS_CLASS },
+      titleBar_sCssClass: GameWindow.TITLE_BAR_S_CSS_CLASS,
+      title_sCssClass: GameWindow.TITLE_S_CSS_CLASS
+    }
   )
   {
-    super.create
-    (
-      {
-        window_sCssClass: window_sCssClass,
-        content_sCssClass: content_sCssClass,
-        titleBar_sCssClass: titleBar_sCssClass,
-        title_sCssClass: title_sCssClass,
-      }
-    );
+    super.create(param);
   }
 
   // --------------- Protected methods ------------------
