8a84cedd77036e78bbabf0abba436f5b806c9ba0
rahman <Rahman@seznam.cz>
2017-09-30 13:53:59 +0200

    Moved code from static classes to respective packet classes.

--- src/server/lib/protocol/CharselectRequest.ts
+++ src/server/lib/protocol/CharselectRequest.ts
@@ -15,10 +15,19 @@
 'use strict';
 
 import {ERROR} from '../../../shared/lib/error/ERROR';
-import {Connection} from '../../../server/lib/connection/Connection';
-import {Charselect} from '../../../server/game/character/Charselect';
+import {Syslog} from '../../../shared/lib/log/Syslog';
 import {CharselectRequest as SharedCharselectRequest} from
   '../../../shared/lib/protocol/CharselectRequest';
+import {AdminLevel} from '../../../shared/lib/admin/AdminLevel';
+import {ServerEntities} from '../../../server/lib/entity/ServerEntities';
+import {Account} from '../../../server/lib/account/Account';
+import {Character} from '../../../server/game/character/Character';
+import {Entity} from '../../../shared/lib/entity/Entity';
+import {MessageType} from '../../../shared/lib/message/MessageType';
+import {Connection} from '../../../server/lib/connection/Connection';
+import {EntityMove} from '../../../shared/lib/protocol/EntityMove';
+import {CharselectResponse} from
+  '../../../shared/lib/protocol/CharselectResponse';
 import {Classes} from '../../../shared/lib/class/Classes';
 
 export class CharselectRequest extends SharedCharselectRequest
@@ -30,14 +39,183 @@ export class CharselectRequest extends SharedCharselectRequest
     this.version = 0;
   }
 
-  // ----------------- Public data ----------------------
-
   // ---------------- Public methods --------------------
 
   // ~ Overrides Packet.process().
   public async process(connection: Connection)
   {
-    Charselect.processRequest(this, connection);
+    let account = this.getAccountFromConnection(connection);
+    let character = this.getRequestedCharacter(connection);
+
+    if (!character || !account)
+      return;
+
+    // Character will be selected when user enters charselect window.
+    account.data.lastActiveCharacter = character;
+    
+    let move = character.enterWorld();
+
+    this.acceptRequest(connection, account, character, move);
+  }
+
+  // --------------- Private methods --------------------
+
+  private isConnectionValid(connection: Connection)
+  {
+    if (!connection)
+    {
+      ERROR("Invalid connection. Charselect request is not processed");
+
+      this.sendErrorResponse
+      (
+        "[ERROR]: Invalid connection.",
+        connection
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
+  private getAccountFromConnection(connection: Connection)
+  {
+    if (!this.isConnectionValid(connection))
+      return null;
+
+    let account = connection.account;
+
+    if (account === null)
+    {
+      ERROR("Charselect request is triggered on connection that has"
+        + " no account attached to it. Request is not processed");
+      return null;
+    }
+
+    if (!Entity.isValid(account))
+    {
+      ERROR("Invalid account on connection. Charselect request is not"
+        + " processed");
+      return null;
+    }
+
+    return account;
+  }
+
+  private getRequestedCharacterId(connection: Connection)
+  {
+    let id = this.characterId;
+    
+    if (!id)
+    {
+      ERROR("Invalid character id in charselect request."
+        + " Request is not processed");
+
+      this.sendErrorResponse
+      (
+        "[ERROR]: Invalid character id.",
+        connection
+      );
+
+      return null;
+    }
+
+    return id;
+  }
+
+  private getRequestedCharacter(connection: Connection)
+  {
+    let id = this.getRequestedCharacterId(connection);
+
+    if (!id)
+      return;
+
+    // Character should already be loaded at this time
+    // (all characters are loaded when account is loaded)
+    // so we just request it from Entities.
+    let character = ServerEntities.get(id);
+
+    if (!character)
+    {
+      ERROR("Invalid character requested by id " + id + "."
+        + " Request is not processed");
+
+      this.sendErrorResponse
+      (
+        "[ERROR]: Invalid character.",
+        connection
+      );
+
+      return null;
+    }
+    
+    return character.dynamicCast(Character);
+  }
+
+  private sendErrorResponse
+  (
+    problem: string,
+    connection: Connection
+  )
+  {
+    this.denyRequest
+    (
+      problem,
+      CharselectResponse.Result.ERROR,
+      connection
+    );
+  }
+
+  private denyRequest
+  (
+    problem: string,
+    result: CharselectResponse.Result,
+    connection: Connection
+  )
+  {
+    let response = new CharselectResponse();
+    
+    response.result = result;
+    response.setProblem(problem);
+
+    connection.send(response);
+  }
+
+  private createOkResponse
+  (
+    account: Account,
+    character: Character,
+    move: EntityMove
+  )
+  {
+    let response = new CharselectResponse();
+
+    response.result = CharselectResponse.Result.OK;
+    response.characterMove = move;
+    response.setLoadLocation(character.getLoadLocation());
+
+    return response;
+  }
+
+  private acceptRequest
+  (
+    connection: Connection,
+    account: Account,
+    character: Character,
+    move: EntityMove
+  )
+  {
+    let response = this.createOkResponse(account, character, move);
+
+    Syslog.log
+    (
+      account.getUserInfo() + " has entered"
+        + " game as " + character.getName(),
+      MessageType.SYSTEM_INFO,
+      AdminLevel.IMMORTAL
+    );
+    
+    connection.send(response);
   }
 }
 
--- src/server/lib/protocol/RegisterRequest.ts
+++ src/server/lib/protocol/RegisterRequest.ts
@@ -15,10 +15,21 @@
 'use strict';
 
 import {ERROR} from '../../../shared/lib/error/ERROR';
-import {Connection} from '../../../server/lib/connection/Connection';
-import {Register} from '../../../server/lib/account/Register';
 import {RegisterRequest as SharedRegisterRequest} from
-  '../../../shared/lib/protocol/RegisterRequest';
+'../../../shared/lib/protocol/RegisterRequest';
+import {Utils} from '../../../shared/lib/utils/Utils';
+import {ServerUtils} from '../../../server/lib/utils/ServerUtils';
+import {Syslog} from '../../../shared/lib/log/Syslog';
+import {AdminLevel} from '../../../shared/lib/admin/AdminLevel';
+import {Serializable} from '../../../shared/lib/class/Serializable';
+import {Message} from '../../../server/lib/message/Message';
+import {MessageType} from '../../../shared/lib/message/MessageType';
+import {Entity} from '../../../shared/lib/entity/Entity';
+import {ServerEntities} from '../../../server/lib/entity/ServerEntities';
+import {Account} from '../../../server/lib/account/Account';
+import {Accounts} from '../../../server/lib/account/Accounts';
+import {Connection} from '../../../server/lib/connection/Connection';
+import {RegisterResponse} from '../../../shared/lib/protocol/RegisterResponse';
 import {Classes} from '../../../shared/lib/class/Classes';
 
 export class RegisterRequest extends SharedRegisterRequest
@@ -35,7 +46,209 @@ export class RegisterRequest extends SharedRegisterRequest
   // ~ Overrides Packet.process().
   public async process(connection: Connection)
   {
-    Register.processRequest(this, connection);
+    if (!this.isRequestValid(connection))
+      return;
+
+    await this.createAccount
+    (
+      // Email is used as account name.
+      this.email,
+      // Only hash is stored, not original password.
+      ServerUtils.md5hash(this.password),
+      connection
+    );
+  }
+
+  // --------------- Private methods --------------------
+
+  private reportAccountCreationFailure
+  (
+    account: Account,
+    accountName,
+    connection: Connection
+  )
+  {
+    this.denyRequest
+    (
+      "[ERROR]: Failed to create account.\n\n"
+        + Message.ADMINS_WILL_FIX_IT,
+      RegisterResponse.Result.FAILED_TO_CREATE_ACCOUNT,
+      connection
+    );
+
+    ERROR("Failed to create account '" + accountName + "'");
+  }
+
+  private async accountAlreadyExists
+  (
+    accountName: string,
+    connection: Connection
+  )
+  : Promise<boolean>
+  {
+    if (await Accounts.isTaken(accountName))
+    {
+      this.denyRequest
+      (
+        "An account is already registered to this e-mail address.",
+        RegisterResponse.Result.EMAIL_PROBLEM,
+        connection
+      );
+  
+      Syslog.log
+      (
+        "Attempt to register account " + accountName
+          + " from " + connection.getOrigin() + " which"
+          + " is already registered",
+        MessageType.CONNECTION_INFO,
+        AdminLevel.IMMORTAL
+      );   
+      return true;
+    }
+
+    return false;
+  }
+
+  private async createAccount
+  (
+    accountName: string,
+    passwordHash: string,
+    connection: Connection
+  )
+  {
+    if (await this.accountAlreadyExists(accountName, connection))
+      return;
+
+    let account = await ServerEntities.createInstanceEntity
+    (
+      Account,
+      Account.name,   // Prototype name.
+      accountName,
+      Entity.NameCathegory.ACCOUNT,
+      passwordHash
+    );
+
+    if (account === null)
+    {
+      this.reportAccountCreationFailure(account, accountName, connection);
+      return;
+    }
+
+    this.initAccount(account, passwordHash, connection);
+    await ServerEntities.save(account);
+    this.acceptRequest(account, connection);
+  }
+
+  private initAccount
+  (
+    account: Account,
+    passwordHash: string,
+    connection: Connection
+  )
+  {
+    account.setPasswordHash(passwordHash);
+    account.addToLists();
+
+    account.attachConnection(connection);
+  }
+
+  private acceptRequest(account: Account, connection: Connection)
+  {
+    let response = new RegisterResponse();
+    response.result = RegisterResponse.Result.OK;
+    
+    // Add newly created account to the response.
+    response.account.serializeEntity
+    (
+      account,
+      Serializable.Mode.SEND_TO_CLIENT
+    );
+
+    Syslog.log
+    (
+      "New player: " + account.getUserInfo(),
+      MessageType.SYSTEM_INFO,
+      AdminLevel.IMMORTAL
+    );
+    
+    connection.send(response);
+  }
+
+  private denyRequest
+  (
+    problem: string,
+    result: RegisterResponse.Result,
+    connection: Connection
+  )
+  {
+    let response = new RegisterResponse();
+
+    response.result = result;
+    response.setProblem(problem);
+
+    connection.send(response);
+  }
+
+  private isEmailValid(connection: Connection): boolean
+  {
+    let problem = this.getEmailProblem();
+
+    if (!problem)
+      return true;
+
+    this.denyRequest
+    (
+      problem,
+      RegisterResponse.Result.EMAIL_PROBLEM,
+      connection
+    );
+
+    Syslog.log
+    (
+      "Attempt to register invalid e-mail address " + this.email + "."
+        + " Problem: " + problem,
+      MessageType.CONNECTION_INFO,
+      AdminLevel.IMMORTAL
+    );
+    
+    return false;
+  }
+
+  private isPasswordValid(connection: Connection): boolean
+  {
+    let problem = this.getEmailProblem();
+
+    if (!problem)
+      return true;
+
+    this.denyRequest
+    (
+      problem,
+      RegisterResponse.Result.PASSWORD_PROBLEM,
+      connection
+    );
+
+    Syslog.log
+    (
+      "Attempt to register account " + this.email
+        + " " + connection.getOrigin() + " using"
+        + " invalid password. Problem: " + problem,
+      MessageType.CONNECTION_INFO,
+      AdminLevel.IMMORTAL
+    );
+
+    return false;
+  }
+
+  private isRequestValid(connection: Connection): boolean
+  {
+    if (!this.isEmailValid(connection))
+      return false;
+
+    if (!this.isPasswordValid(connection))
+      return false;
+
+    return true;
   }
 }
 
