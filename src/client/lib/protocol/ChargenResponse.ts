5a3804b6209291bbd1b83901fcbf805933202617
Rahman <Rahman@seznam.cz>
2017-10-01 17:25:42 +0200

    Fixed chargen error.

--- src/client/lib/protocol/ChargenResponse.ts
+++ src/client/lib/protocol/ChargenResponse.ts
@@ -16,6 +16,7 @@
 
 import {ERROR} from '../../../shared/lib/error/ERROR';
 import {ClientApp} from '../../../client/lib/app/ClientApp';
+import {ClientEntities} from '../../../client/lib/entity/ClientEntities';
 import {Account} from '../../../client/lib/account/Account';
 import {Character} from '../../../client/game/character/Character';
 import {Windows} from '../../../client/gui/window/Windows';
@@ -62,6 +63,9 @@ export class ChargenResponse extends SharedChargenResponse
   {
     let character = this.extractData(connection);
 
+    if (!character)
+      return;
+
     ClientApp.setState(ClientApp.State.CHARSELECT);
 
     // Select newly added character
@@ -70,13 +74,32 @@ export class ChargenResponse extends SharedChargenResponse
     Windows.charselectWindow.form.selectCharacter(character.getId());
   }
 
+  // -> Returns 'null' on failure.
   private extractData(connection: Connection)
   {
+    // This should overwrite existing account
+    // (we don't need to update connection.account because
+    //  player had to log in before creating a character
+    //  so that reference is already set).
     let account = this.account.deserializeEntity(Account);
+
+    if (connection.getAccount().getId() !== account.getId())
+    {
+      ERROR("Account (" + account.getErrorIdString() + ")"
+        + " contained in chargen response packet is"
+        + " not the one the player logged in with"
+        + " (" + connection.getAccount().getErrorIdString() + ")."
+        + " Account is not updated and response isn't accepted");
+
+      // There is probably no point in trying to recover from this
+      // error so this is just for for the sake of trying.
+      ClientEntities.release(account);
+      return null;
+    }
+
     let character = this.character.deserializeEntity(Character);
 
     account.data.updateCharacterReference(character);
-    connection.setAccount(account);
 
     return character;
   }
